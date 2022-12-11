f5c566df114b4d8188d15d313ccb21098876ccf1
Agos Favalli <agostinafavalli@hotmail.com>
2022-12-11 19:12:30 -0300

    Segunda entrega

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+//Variables para mensaje de Bienvenida
 
 let nombre = prompt("Ingrese su Nombre");
 let apellido = prompt("Ingrese su apellido");
@@ -8,11 +8,41 @@ let mensaje = "Bienvenid@" + " " + nombre + " " + apellido ;
 
 if ( (nombre != " " && apellido != " ") ) {
     alert(mensaje);
+    
+    //Variables de productos impresos por consola
+
+function Producto(nombre, precio, disponible) {
+    this.nombre = nombre;
+    this.precio = precio;
+    this.disponible = disponible; 
+}
+const producto1 = new Producto("Abridores Coronita", 199, "Hay Stock");
+const producto2 = new Producto("Abridores Mariposa", 208, "Hay Stock");
+const producto3 = new Producto("Abridores Mariposa Calada", 199, "Hay Stock");
+const producto4 = new Producto("Anillo Vive Rie Ama", 376, "Hay Stock");
+const producto5 = new Producto("Anillo Vive tus Sueños", 376, "Hay Stock");
+const producto6 = new Producto("Anillo Se Feliz", 376, "No hay Stock");
+const producto7 = new Producto("Anillo Estrella", 376, "No hay Stock");
+const producto8 = new Producto("Anillo Circulo", 376, "Hay Stock");
+const producto9 = new Producto("Anillo Corazon", 376, "Hay hay Stock");
+const producto10 = new Producto("Anillo Ola", 356, "Hay Stock")
+
+console.log(producto1, producto2, producto3, producto4, producto5, producto6, producto7, producto8, producto9, producto10)
+
+//Arrays
+
+let carrito =["producto1", "producto2", "producto3", "producto4", "producto5", "producto6", "producto7", "producto8", "producto9", "producto10"];
+    carrito.splice(5, 2)
+    alert(carrito.length); 
+
+//Variables para calculos
 
     let minimo = 4000
     let totalAPagar = prompt ("Ingrese el importe a abonar");
     let envioGratis = 7000
     let cuotas = 10000
+
+    //Funciones para cálculo de formas de pagos
 
     if (totalAPagar < minimo) {
         alert("Su importe es inferior al mínimo de compra exigido por $4000");
@@ -64,6 +94,8 @@ if ( (nombre != " " && apellido != " ") ) {
   else {
        alert("error al ingresar datos")
   }
+
+  
 
  
 
